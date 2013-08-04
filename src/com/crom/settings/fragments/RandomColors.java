CarbonDev/android_packages_apps_CarbonFibers

Add NavBar and Toggles Settings.
Navring setting FC's. Needs fixed.
Navbar seems to also overlap stuff. Need to look into that also.

More settings can be added for stuff that is already in frameworks/base.

Change-Id: I697face4b81ea0bb7f6af9d7e0ceca3e3aa81d0a

src/com/carbon/settings/fragments/RandomColors.java
@@ -1,0 +1,116 @@
+
+package com.carbon.settings.fragments;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.carbon.settings.R;
+import com.carbon.settings.SettingsPreferenceFragment;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class RandomColors extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String PREF_RANDOM_COLOR_ONE = "color_one";
+    private static final String PREF_RANDOM_COLOR_TWO = "color_two";
+    private static final String PREF_RANDOM_COLOR_THREE = "color_three";
+    private static final String PREF_RANDOM_COLOR_FOUR = "color_four";
+    private static final String PREF_RANDOM_COLOR_FIVE = "color_five";
+    private static final String PREF_RANDOM_COLOR_SIX = "color_six";
+
+    ColorPickerPreference mOne;
+    ColorPickerPreference mTwo;
+    ColorPickerPreference mThree;
+    ColorPickerPreference mFour;
+    ColorPickerPreference mFive;
+    ColorPickerPreference mSix;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.random_colors);
+
+        mOne = (ColorPickerPreference) findPreference(PREF_RANDOM_COLOR_ONE);
+        mTwo = (ColorPickerPreference) findPreference(PREF_RANDOM_COLOR_TWO);
+        mThree = (ColorPickerPreference) findPreference(PREF_RANDOM_COLOR_THREE);
+        mFour = (ColorPickerPreference) findPreference(PREF_RANDOM_COLOR_FOUR);
+        mFive = (ColorPickerPreference) findPreference(PREF_RANDOM_COLOR_FIVE);
+        mSix = (ColorPickerPreference) findPreference(PREF_RANDOM_COLOR_SIX);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerListeners();
+    }
+
+    private void registerListeners() {
+        mOne.setOnPreferenceChangeListener(this);
+        mTwo.setOnPreferenceChangeListener(this);
+        mThree.setOnPreferenceChangeListener(this);
+        mFour.setOnPreferenceChangeListener(this);
+        mFive.setOnPreferenceChangeListener(this);
+        mSix.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mOne) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mContentRes,
+                    Settings.System.RANDOM_COLOR_ONE, intHex);
+            return true;
+        } else if (preference == mTwo) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mContentRes,
+                    Settings.System.RANDOM_COLOR_TWO, intHex);
+            return true;
+        } else if (preference == mThree) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mContentRes,
+                    Settings.System.RANDOM_COLOR_THREE, intHex);
+            return true;
+        } else if (preference == mFour) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mContentRes,
+                    Settings.System.RANDOM_COLOR_FOUR, intHex);
+            return true;
+        } else if (preference == mFive) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mContentRes,
+                    Settings.System.RANDOM_COLOR_FIVE, intHex);
+            return true;
+        } else if (preference == mSix) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mContentRes,
+                    Settings.System.RANDOM_COLOR_SIX, intHex);
+            return true;
+        }
+        return false;
+    }
+
+}

src/com/carbon/settings/service/ChangeFastChargeStateReceiver.java
@@ -1,0 +1,25 @@
+package com.carbon.settings.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import java.io.File;
+
+public class ChangeFastChargeStateReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final String fCHargePath = context
+                .getString(com.android.internal.R.string.config_fastChargePath);
+        if (fCHargePath == null || fCHargePath.isEmpty() || !new File(fCHargePath).exists()) {
+            Log.e("ROMControl", "Attempted to change fast charge state but it's not enabled?");
+            return;
+        }
+        final String value = intent.getBooleanExtra("newState", false) ? "1" : "0";
+        context.startService(new Intent(context,
+                ExternalCommandService.class)
+                .putExtra("cmd", "echo " + value + " > " + fCHargePath));
+    }
+}

flesh75/android_packages_apps_C-Tool

Conflicts:
	res/values/dimens.xml

src/com/crom/settings/fragments/RandomColors.java
@@ -1,0 +1,116 @@
+
+package com.crom.settings.fragments;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.crom.settings.R;
+import com.crom.settings.SettingsPreferenceFragment;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class RandomColors extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String PREF_RANDOM_COLOR_ONE = "color_one";
+    private static final String PREF_RANDOM_COLOR_TWO = "color_two";
+    private static final String PREF_RANDOM_COLOR_THREE = "color_three";
+    private static final String PREF_RANDOM_COLOR_FOUR = "color_four";
+    private static final String PREF_RANDOM_COLOR_FIVE = "color_five";
+    private static final String PREF_RANDOM_COLOR_SIX = "color_six";
+
+    ColorPickerPreference mOne;
+    ColorPickerPreference mTwo;
+    ColorPickerPreference mThree;
+    ColorPickerPreference mFour;
+    ColorPickerPreference mFive;
+    ColorPickerPreference mSix;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.random_colors);
+
+        mOne = (ColorPickerPreference) findPreference(PREF_RANDOM_COLOR_ONE);
+        mTwo = (ColorPickerPreference) findPreference(PREF_RANDOM_COLOR_TWO);
+        mThree = (ColorPickerPreference) findPreference(PREF_RANDOM_COLOR_THREE);
+        mFour = (ColorPickerPreference) findPreference(PREF_RANDOM_COLOR_FOUR);
+        mFive = (ColorPickerPreference) findPreference(PREF_RANDOM_COLOR_FIVE);
+        mSix = (ColorPickerPreference) findPreference(PREF_RANDOM_COLOR_SIX);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerListeners();
+    }
+
+    private void registerListeners() {
+        mOne.setOnPreferenceChangeListener(this);
+        mTwo.setOnPreferenceChangeListener(this);
+        mThree.setOnPreferenceChangeListener(this);
+        mFour.setOnPreferenceChangeListener(this);
+        mFive.setOnPreferenceChangeListener(this);
+        mSix.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mOne) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mContentRes,
+                    Settings.System.RANDOM_COLOR_ONE, intHex);
+            return true;
+        } else if (preference == mTwo) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mContentRes,
+                    Settings.System.RANDOM_COLOR_TWO, intHex);
+            return true;
+        } else if (preference == mThree) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mContentRes,
+                    Settings.System.RANDOM_COLOR_THREE, intHex);
+            return true;
+        } else if (preference == mFour) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mContentRes,
+                    Settings.System.RANDOM_COLOR_FOUR, intHex);
+            return true;
+        } else if (preference == mFive) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mContentRes,
+                    Settings.System.RANDOM_COLOR_FIVE, intHex);
+            return true;
+        } else if (preference == mSix) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mContentRes,
+                    Settings.System.RANDOM_COLOR_SIX, intHex);
+            return true;
+        }
+        return false;
+    }
+
+}

src/com/crom/settings/service/ChangeFastChargeStateReceiver.java
@@ -1,0 +1,25 @@
+package com.crom.settings.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import java.io.File;
+
+public class ChangeFastChargeStateReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final String fCHargePath = context
+                .getString(com.android.internal.R.string.config_fastChargePath);
+        if (fCHargePath == null || fCHargePath.isEmpty() || !new File(fCHargePath).exists()) {
+            Log.e("ROMControl", "Attempted to change fast charge state but it's not enabled?");
+            return;
+        }
+        final String value = intent.getBooleanExtra("newState", false) ? "1" : "0";
+        context.startService(new Intent(context,
+                ExternalCommandService.class)
+                .putExtra("cmd", "echo " + value + " > " + fCHargePath));
+    }
+}