feea99e4097c02c4ac0fdf8b47a1d5f2f9e3cf29
Daniel MM <daxx4182@gmail.com>
2013-08-18 20:26:43 +0800

    Device Tools
    Initially for HTC then Samsung

---
+++ src/com/crom/settings/device/DeviceStartup.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2012-2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crom.settings.device;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.crom.settings.device.htc.ButtonLightFragmentActivity;
+import com.crom.settings.device.htc.Logo2MenuSwitch;
+import com.crom.settings.device.htc.WakeMethod;
+import com.crom.settings.device.htc.LongTapLogoSleepSwitch;
+import com.crom.settings.device.htc.ButtonLightNotificationSwitch;
+import com.crom.settings.device.htc.BacklightDisableSwitch;
+import com.crom.settings.device.htc.PocketDetectionMethod;
+import com.crom.settings.device.htc.SensorsFragmentActivity;
+import com.crom.settings.device.htc.TouchscreenFragmentActivity;
+
+public class DeviceStartup extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, final Intent bootintent) {
+        // HTC One
+        SensorsFragmentActivity.restore(context);
+        TouchscreenFragmentActivity.restore(context);
+        Logo2MenuSwitch.restore(context);
+        WakeMethod.restore(context);
+        LongTapLogoSleepSwitch.restore(context);
+        ButtonLightFragmentActivity.restore(context);
+        ButtonLightNotificationSwitch.restore(context);
+        PocketDetectionMethod.restore(context);
+        BacklightDisableSwitch.restore(context);
+
+        // Samsung
+          // nothing for now
+    }
+}
