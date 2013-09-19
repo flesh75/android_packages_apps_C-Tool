CarbonDev/android_packages_apps_CarbonFibers

Interface Tabs, AD: Option to show Date, improvements

src/com/carbon/settings/fragments/inter/BacklightTimeoutSeekBar.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.carbon.settings.fragments;
+package com.carbon.settings.fragments.inter;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;

src/com/carbon/settings/fragments/inter/ButtonBacklightBrightness.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.carbon.settings.fragments;
+package com.carbon.settings.fragments.inter;
 
 import android.app.AlertDialog;
 import android.content.ContentResolver;

src/com/carbon/settings/fragments/inter/InterfaceButtons.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.carbon.settings.fragments;
+package com.carbon.settings.fragments.inter;
 
 import android.content.ContentResolver;
 import android.content.res.Resources;
@@ -31,7 +31,7 @@ import com.carbon.settings.R;
 import com.carbon.settings.SettingsPreferenceFragment;
 import com.carbon.settings.Utils;
 
-public class ButtonSettings extends SettingsPreferenceFragment implements
+public class InterfaceButtons extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String KEY_ENABLE_CUSTOM_BINDING = "hardware_keys_enable_custom";

src/com/carbon/settings/fragments/inter/InterfaceDensity.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.carbon.settings.fragments;
+package com.carbon.settings.fragments.inter;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -44,7 +44,7 @@ import com.carbon.settings.util.CMDProcessor;
 import com.carbon.settings.util.CMDProcessor.CommandResult;
 import com.carbon.settings.util.Helpers;
 
-public class DensityChanger extends SettingsPreferenceFragment implements
+public class InterfaceDensity extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String TAG = "DensityChanger";

src/com/carbon/settings/fragments/inter/InterfaceRecents.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.carbon.settings.fragments;
+package com.carbon.settings.fragments.inter;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -40,10 +40,11 @@ import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 import java.util.Date;
 
-public class RamBar extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+public class InterfaceRecents extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
-    private static final String TAG = "RamBar";
+    private static final String TAG = "Recents";
 
+    private static final String KEY_RECENTS_ASSIST = "recents_target_assist";
     private static final String RAM_BAR_MODE = "ram_bar_mode";
     private static final String RAM_BAR_COLOR_APP_MEM = "ram_bar_color_app_mem";
     private static final String RAM_BAR_COLOR_CACHE_MEM = "ram_bar_color_cache_mem";
@@ -55,6 +56,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
     static final int DEFAULT_CACHE_COLOR = 0xff00aa00;
     static final int DEFAULT_ACTIVE_APPS_COLOR = 0xff33b5e5;
 
+    private CheckBoxPreference mShowAssistButton;
     private ListPreference mRamBarMode;
     private ColorPickerPreference mRamBarAppMemColor;
     private ColorPickerPreference mRamBarCacheMemColor;
@@ -67,9 +69,13 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
         int intColor;
         String hexColor;
 
-        addPreferencesFromResource(R.xml.ram_bar);
+        addPreferencesFromResource(R.xml.interface_recents);
 
         PreferenceScreen prefSet = getPreferenceScreen();
+
+        mShowAssistButton = (CheckBoxPreference) findPreference(KEY_RECENTS_ASSIST);
+        mShowAssistButton.setChecked(Settings.System.getInt(mContentRes,
+                Settings.System.RECENTS_TARGET_ASSIST, 0) == 1);
 
         mRamBarMode = (ListPreference) prefSet.findPreference(RAM_BAR_MODE);
         int ramBarMode = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
@@ -80,7 +86,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
 
         mRamBarAppMemColor = (ColorPickerPreference) findPreference(RAM_BAR_COLOR_APP_MEM);
         mRamBarAppMemColor.setOnPreferenceChangeListener(this);
-        intColor = Settings.System.getInt(getActivity().getContentResolver(),
+        intColor = Settings.System.getInt(mContentAppRes,
                     Settings.System.RECENTS_RAM_BAR_ACTIVE_APPS_COLOR, DEFAULT_ACTIVE_APPS_COLOR);
         hexColor = String.format("#%08x", (0xffffffff & intColor));
         mRamBarAppMemColor.setSummary(hexColor);
@@ -88,7 +94,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
 
         mRamBarCacheMemColor = (ColorPickerPreference) findPreference(RAM_BAR_COLOR_CACHE_MEM);
         mRamBarCacheMemColor.setOnPreferenceChangeListener(this);
-        intColor = Settings.System.getInt(getActivity().getContentResolver(),
+        intColor = Settings.System.getInt(mContentAppRes,
                     Settings.System.RECENTS_RAM_BAR_CACHE_COLOR, DEFAULT_CACHE_COLOR);
         hexColor = String.format("#%08x", (0xffffffff & intColor));
         mRamBarCacheMemColor.setSummary(hexColor);
@@ -96,7 +102,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
 
         mRamBarTotalMemColor = (ColorPickerPreference) findPreference(RAM_BAR_COLOR_TOTAL_MEM);
         mRamBarTotalMemColor.setOnPreferenceChangeListener(this);
-        intColor = Settings.System.getInt(getActivity().getContentResolver(),
+        intColor = Settings.System.getInt(mContentAppRes,
                     Settings.System.RECENTS_RAM_BAR_MEM_COLOR, DEFAULT_MEM_COLOR);
         hexColor = String.format("#%08x", (0xffffffff & intColor));
         mRamBarTotalMemColor.setSummary(hexColor);
@@ -146,7 +152,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
             preference.setSummary(hex);
 
             int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getActivity().getContentResolver(),
+            Settings.System.putInt(mContentAppRes,
                     Settings.System.RECENTS_RAM_BAR_ACTIVE_APPS_COLOR, intHex);
             return true;
         } else if (preference == mRamBarCacheMemColor) {
@@ -155,7 +161,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
             preference.setSummary(hex);
 
             int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getActivity().getContentResolver(),
+            Settings.System.putInt(mContentAppRes,
                     Settings.System.RECENTS_RAM_BAR_CACHE_COLOR, intHex);
             return true;
         } else if (preference == mRamBarTotalMemColor) {
@@ -164,7 +170,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
             preference.setSummary(hex);
 
             int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getActivity().getContentResolver(),
+            Settings.System.putInt(mContentAppRes,
                     Settings.System.RECENTS_RAM_BAR_MEM_COLOR, intHex);
             return true;
          }
@@ -173,15 +179,21 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         boolean value;
+
+        if (preference == mShowAssistButton) {
+            Settings.System.putInt(mContentAppRes,
+                    Settings.System.RECENTS_TARGET_ASSIST,
+            mShowAssistButton.isChecked() ? 1 : 0 );
+        }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     private void ramBarColorReset() {
-        Settings.System.putInt(getActivity().getContentResolver(),
+        Settings.System.putInt(mContentAppRes,
                 Settings.System.RECENTS_RAM_BAR_ACTIVE_APPS_COLOR, DEFAULT_ACTIVE_APPS_COLOR);
-        Settings.System.putInt(getActivity().getContentResolver(),
+        Settings.System.putInt(mContentAppRes,
                 Settings.System.RECENTS_RAM_BAR_CACHE_COLOR, DEFAULT_CACHE_COLOR);
-        Settings.System.putInt(getActivity().getContentResolver(),
+        Settings.System.putInt(mContentAppRes,
                 Settings.System.RECENTS_RAM_BAR_MEM_COLOR, DEFAULT_MEM_COLOR);
 
         mRamBarAppMemColor.setNewPreviewColor(DEFAULT_ACTIVE_APPS_COLOR);
@@ -197,7 +209,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
 
 
     private void updateRamBarOptions() {
-        int ramBarMode = Settings.System.getInt(getActivity().getContentResolver(),
+        int ramBarMode = Settings.System.getInt(mContentAppRes,
                Settings.System.RECENTS_RAM_BAR_MODE, 0);
         if (ramBarMode == 0) {
             mRamBarAppMemColor.setEnabled(false);

src/com/carbon/settings/fragments/inter/InterfaceTab.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (C) 2012-2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.carbon.settings.fragments.inter;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Resources;
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.PreferenceDrawerActivityAlt;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.carbon.settings.R;
+import com.carbon.settings.SettingsPreferenceFragment;
+import com.carbon.settings.Utils;
+
+import com.carbon.settings.fragments.inter.*;
+
+import java.lang.Exception;
+import java.util.ArrayList;
+
+public class InterfaceTab extends SettingsPreferenceFragment {
+
+    private static final String TAG = "Interface_Category";
+
+    PagerTabStrip mPagerTabStrip;
+    ViewPager mViewPager;
+
+    String titleString[];
+
+    ViewGroup mContainer;
+
+    static Bundle mSavedState;
+
+    public boolean hasButtons() {
+        return !getResources().getBoolean(com.android.internal.R.bool.config_showNavigationBar);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        mContainer = container;
+
+        View view = inflater.inflate(R.layout.pager_tab, container, false);
+        mViewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        mPagerTabStrip = (PagerTabStrip) view.findViewById(R.id.pagerTabStrip);
+
+        InterfaceAdapter InterfaceAdapter = new InterfaceAdapter(getFragmentManager());
+        mViewPager.setAdapter(InterfaceAdapter);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // After confirming PreferenceScreen is available, we call super.
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (!Utils.isTablet(getActivity())) {
+            mContainer.setPadding(0, 0, 0, 0);
+        }
+    }
+
+    class InterfaceAdapter extends FragmentPagerAdapter {
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
+
+        public InterfaceAdapter(FragmentManager fm) {
+            super(fm);
+            frags[0] = new InterfaceGeneral();
+            frags[1] = new InterfaceRecents();
+            frags[2] = new InterfaceButtons();
+            frags[3] = new InterfaceDensity();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return frags.length;
+        }
+    }
+
+    private String[] getTitles() {
+        String titleString[];
+        titleString = new String[]{
+                getString(R.string.interface_general_category),
+                getString(R.string.interface_recents_category),
+                getString(R.string.interface_buttons_category),
+                getString(R.string.interface_density_category)};
+        return titleString;
+    }
+}

flesh75/android_packages_apps_C-Tool

Conflicts:
	AndroidManifest.xml
	res/layout/button_backlight.xml
	res/xml/active_display_settings.xml
	res/xml/interface_general.xml
	res/xml/preference_headers.xml
	src/com/crom/settings/fragments/BacklightTimeoutSeekBar.java
	src/com/crom/settings/fragments/ButtonBacklightBrightness.java
	src/com/crom/settings/fragments/ButtonSettings.java
	src/com/crom/settings/fragments/DensityChanger.java
	src/com/crom/settings/fragments/InterfaceSettings.java
	src/com/crom/settings/fragments/RamBar.java
	src/com/crom/settings/fragments/lockscreen/ActiveDisplay.java

src/com/crom/settings/fragments/inter/BacklightTimeoutSeekBar.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.crom.settings.fragments;
+package com.crom.settings.fragments.inter;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;

src/com/crom/settings/fragments/inter/ButtonBacklightBrightness.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.crom.settings.fragments;
+package com.crom.settings.fragments.inter;
 
 import android.app.AlertDialog;
 import android.content.ContentResolver;

src/com/crom/settings/fragments/inter/InterfaceButtons.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.crom.settings.fragments;
+package com.crom.settings.fragments.inter;
 
 import android.content.ContentResolver;
 import android.content.res.Resources;
@@ -31,7 +31,7 @@ import com.crom.settings.R;
 import com.crom.settings.SettingsPreferenceFragment;
 import com.crom.settings.Utils;
 
-public class ButtonSettings extends SettingsPreferenceFragment implements
+public class InterfaceButtons extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String KEY_ENABLE_CUSTOM_BINDING = "hardware_keys_enable_custom";

src/com/crom/settings/fragments/inter/InterfaceDensity.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.crom.settings.fragments;
+package com.crom.settings.fragments.inter;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -44,7 +44,7 @@ import com.crom.settings.util.CMDProcessor;
 import com.crom.settings.util.CMDProcessor.CommandResult;
 import com.crom.settings.util.Helpers;
 
-public class DensityChanger extends SettingsPreferenceFragment implements
+public class InterfaceDensity extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String TAG = "DensityChanger";

src/com/crom/settings/fragments/inter/InterfaceRecents.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.crom.settings.fragments;
+package com.crom.settings.fragments.inter;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -40,10 +40,11 @@ import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 import java.util.Date;
 
-public class RamBar extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+public class InterfaceRecents extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
-    private static final String TAG = "RamBar";
+    private static final String TAG = "Recents";
 
+    private static final String KEY_RECENTS_ASSIST = "recents_target_assist";
     private static final String RAM_BAR_MODE = "ram_bar_mode";
     private static final String RAM_BAR_COLOR_APP_MEM = "ram_bar_color_app_mem";
     private static final String RAM_BAR_COLOR_CACHE_MEM = "ram_bar_color_cache_mem";
@@ -55,6 +56,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
     static final int DEFAULT_CACHE_COLOR = 0xff00aa00;
     static final int DEFAULT_ACTIVE_APPS_COLOR = 0xff33b5e5;
 
+    private CheckBoxPreference mShowAssistButton;
     private ListPreference mRamBarMode;
     private ColorPickerPreference mRamBarAppMemColor;
     private ColorPickerPreference mRamBarCacheMemColor;
@@ -67,9 +69,13 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
         int intColor;
         String hexColor;
 
-        addPreferencesFromResource(R.xml.ram_bar);
+        addPreferencesFromResource(R.xml.interface_recents);
 
         PreferenceScreen prefSet = getPreferenceScreen();
+
+        mShowAssistButton = (CheckBoxPreference) findPreference(KEY_RECENTS_ASSIST);
+        mShowAssistButton.setChecked(Settings.System.getInt(mContentRes,
+                Settings.System.RECENTS_TARGET_ASSIST, 0) == 1);
 
         mRamBarMode = (ListPreference) prefSet.findPreference(RAM_BAR_MODE);
         int ramBarMode = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
@@ -80,7 +86,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
 
         mRamBarAppMemColor = (ColorPickerPreference) findPreference(RAM_BAR_COLOR_APP_MEM);
         mRamBarAppMemColor.setOnPreferenceChangeListener(this);
-        intColor = Settings.System.getInt(getActivity().getContentResolver(),
+        intColor = Settings.System.getInt(mContentAppRes,
                     Settings.System.RECENTS_RAM_BAR_ACTIVE_APPS_COLOR, DEFAULT_ACTIVE_APPS_COLOR);
         hexColor = String.format("#%08x", (0xffffffff & intColor));
         mRamBarAppMemColor.setSummary(hexColor);
@@ -88,7 +94,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
 
         mRamBarCacheMemColor = (ColorPickerPreference) findPreference(RAM_BAR_COLOR_CACHE_MEM);
         mRamBarCacheMemColor.setOnPreferenceChangeListener(this);
-        intColor = Settings.System.getInt(getActivity().getContentResolver(),
+        intColor = Settings.System.getInt(mContentAppRes,
                     Settings.System.RECENTS_RAM_BAR_CACHE_COLOR, DEFAULT_CACHE_COLOR);
         hexColor = String.format("#%08x", (0xffffffff & intColor));
         mRamBarCacheMemColor.setSummary(hexColor);
@@ -96,7 +102,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
 
         mRamBarTotalMemColor = (ColorPickerPreference) findPreference(RAM_BAR_COLOR_TOTAL_MEM);
         mRamBarTotalMemColor.setOnPreferenceChangeListener(this);
-        intColor = Settings.System.getInt(getActivity().getContentResolver(),
+        intColor = Settings.System.getInt(mContentAppRes,
                     Settings.System.RECENTS_RAM_BAR_MEM_COLOR, DEFAULT_MEM_COLOR);
         hexColor = String.format("#%08x", (0xffffffff & intColor));
         mRamBarTotalMemColor.setSummary(hexColor);
@@ -146,7 +152,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
             preference.setSummary(hex);
 
             int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getActivity().getContentResolver(),
+            Settings.System.putInt(mContentAppRes,
                     Settings.System.RECENTS_RAM_BAR_ACTIVE_APPS_COLOR, intHex);
             return true;
         } else if (preference == mRamBarCacheMemColor) {
@@ -155,7 +161,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
             preference.setSummary(hex);
 
             int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getActivity().getContentResolver(),
+            Settings.System.putInt(mContentAppRes,
                     Settings.System.RECENTS_RAM_BAR_CACHE_COLOR, intHex);
             return true;
         } else if (preference == mRamBarTotalMemColor) {
@@ -164,7 +170,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
             preference.setSummary(hex);
 
             int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getActivity().getContentResolver(),
+            Settings.System.putInt(mContentAppRes,
                     Settings.System.RECENTS_RAM_BAR_MEM_COLOR, intHex);
             return true;
          }
@@ -173,15 +179,21 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         boolean value;
+
+        if (preference == mShowAssistButton) {
+            Settings.System.putInt(mContentAppRes,
+                    Settings.System.RECENTS_TARGET_ASSIST,
+            mShowAssistButton.isChecked() ? 1 : 0 );
+        }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     private void ramBarColorReset() {
-        Settings.System.putInt(getActivity().getContentResolver(),
+        Settings.System.putInt(mContentAppRes,
                 Settings.System.RECENTS_RAM_BAR_ACTIVE_APPS_COLOR, DEFAULT_ACTIVE_APPS_COLOR);
-        Settings.System.putInt(getActivity().getContentResolver(),
+        Settings.System.putInt(mContentAppRes,
                 Settings.System.RECENTS_RAM_BAR_CACHE_COLOR, DEFAULT_CACHE_COLOR);
-        Settings.System.putInt(getActivity().getContentResolver(),
+        Settings.System.putInt(mContentAppRes,
                 Settings.System.RECENTS_RAM_BAR_MEM_COLOR, DEFAULT_MEM_COLOR);
 
         mRamBarAppMemColor.setNewPreviewColor(DEFAULT_ACTIVE_APPS_COLOR);
@@ -197,7 +209,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
 
 
     private void updateRamBarOptions() {
-        int ramBarMode = Settings.System.getInt(getActivity().getContentResolver(),
+        int ramBarMode = Settings.System.getInt(mContentAppRes,
                Settings.System.RECENTS_RAM_BAR_MODE, 0);
         if (ramBarMode == 0) {
             mRamBarAppMemColor.setEnabled(false);

src/com/crom/settings/fragments/inter/InterfaceTab.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (C) 2012-2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crom.settings.fragments.inter;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Resources;
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.PreferenceDrawerActivityAlt;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.crom.settings.R;
+import com.crom.settings.SettingsPreferenceFragment;
+import com.crom.settings.Utils;
+
+import com.crom.settings.fragments.inter.*;
+
+import java.lang.Exception;
+import java.util.ArrayList;
+
+public class InterfaceTab extends SettingsPreferenceFragment {
+
+    private static final String TAG = "Interface_Category";
+
+    PagerTabStrip mPagerTabStrip;
+    ViewPager mViewPager;
+
+    String titleString[];
+
+    ViewGroup mContainer;
+
+    static Bundle mSavedState;
+
+    public boolean hasButtons() {
+        return !getResources().getBoolean(com.android.internal.R.bool.config_showNavigationBar);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        mContainer = container;
+
+        View view = inflater.inflate(R.layout.pager_tab, container, false);
+        mViewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        mPagerTabStrip = (PagerTabStrip) view.findViewById(R.id.pagerTabStrip);
+
+        InterfaceAdapter InterfaceAdapter = new InterfaceAdapter(getFragmentManager());
+        mViewPager.setAdapter(InterfaceAdapter);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // After confirming PreferenceScreen is available, we call super.
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (!Utils.isTablet(getActivity())) {
+            mContainer.setPadding(0, 0, 0, 0);
+        }
+    }
+
+    class InterfaceAdapter extends FragmentPagerAdapter {
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
+
+        public InterfaceAdapter(FragmentManager fm) {
+            super(fm);
+            frags[0] = new InterfaceGeneral();
+            frags[1] = new InterfaceRecents();
+            frags[2] = new InterfaceButtons();
+            frags[3] = new InterfaceDensity();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return frags.length;
+        }
+    }
+
+    private String[] getTitles() {
+        String titleString[];
+        titleString = new String[]{
+                getString(R.string.interface_general_category),
+                getString(R.string.interface_recents_category),
+                getString(R.string.interface_buttons_category),
+                getString(R.string.interface_density_category)};
+        return titleString;
+    }
+}