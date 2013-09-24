3a910abebe7bb7afd61b50031df0fcfca8355c01
maxwen <max.weninger@gmail.com>
2013-09-24 20:56:53 +0200

    RC: Wakelock blocker (2/2) - squashed
    Change-Id: Ie319d6db8032c42238b3576828c2e0970a52812d
    
    Conflicts:
    	res/values/strings.xml
    	src/com/crom/settings/CreateShortcut.java

--- src/com/crom/settings/CreateShortcut.java
+++ src/com/crom/settings/CreateShortcut.java
@@ -74,6 +74,8 @@ public class CreateShortcut extends LauncherActivity {
             return R.drawable.ic_crom_lockscreen;
         else if (c.equals("ScreenStateToggles"))
             return R.drawable.ic_crom_screen_state;
+        else if (c.equals("WakeLockBlocker")){
+            return R.drawable.ic_crom_wakelock_blocker;
         else
             return R.mipmap.ic_launcher;
     }
